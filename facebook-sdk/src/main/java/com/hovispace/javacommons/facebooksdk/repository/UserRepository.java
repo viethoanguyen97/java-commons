40842b55bb1bfa8b75a4a2d3e27ddd9dcab45ebc
Hoa Nguyen <hoa.nguyen@episerver.com>
2020-07-10 17:39:28 +0700

    remove facebook-sdk module

--- facebook-sdk/src/main/java/com/hovispace/javacommons/facebooksdk/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.hovispace.javacommons.facebooksdk.repository;
-
-import com.hovispace.javacommons.facebooksdk.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-    User findByUsername(String username);
-}
